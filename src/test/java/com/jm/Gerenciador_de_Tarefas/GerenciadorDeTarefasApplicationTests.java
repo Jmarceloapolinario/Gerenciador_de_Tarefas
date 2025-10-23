33c64a758667b828fcff53241724b8fcec04d59d
Jose Marcelo <122740274+Jmarceloapolinario@users.noreply.github.com>
2025-10-23 20:07:43 -0300

    Initial commit

---
+++ src/test/java/com/jm/Gerenciador_de_Tarefas/GerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.Gerenciador_de_Tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
