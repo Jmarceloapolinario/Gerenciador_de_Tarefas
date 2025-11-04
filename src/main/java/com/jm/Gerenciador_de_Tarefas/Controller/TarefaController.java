3ed32e39e5112918052b74ba7952ebbc23d7602c
Jose Marcelo <122740274+Jmarceloapolinario@users.noreply.github.com>
2025-11-04 10:20:13 -0300

    doc: Usando Swagger para documentar a api

--- src/main/java/com/jm/Gerenciador_de_Tarefas/Controller/TarefaController.java
+++ src/main/java/com/jm/Gerenciador_de_Tarefas/Controller/TarefaController.java
@@ -4,6 +4,9 @@ package com.jm.Gerenciador_de_Tarefas.Controller;
 import com.jm.Gerenciador_de_Tarefas.DTO.TarefasDTO;
 import com.jm.Gerenciador_de_Tarefas.Model.TarefaModel;
 import com.jm.Gerenciador_de_Tarefas.Service.TarefaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +25,18 @@ public class TarefaController {
         this.tarefaService = tarefaService;
     }
     @GetMapping("/listar")
+    @Operation(summary = "Lista todas as tarefas")
     public ResponseEntity<List<TarefasDTO>> listartarefas(){
         List<TarefasDTO> tarefas = tarefaService.listarTarefas();
         return ResponseEntity.ok(tarefas);
 
 }
     @GetMapping("/listar/{data}")
+    @Operation(summary = "Lista todas as tarefas por data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa encontrada"),
+            @ApiResponse(responseCode = "400", description = "Tarefa nao encotarada")
+    })
     public ResponseEntity<?> listaTarefasData(@PathVariable LocalDate data) {
         List<TarefasDTO> tarefa = tarefaService.listarTarefasDate(data);
         if (tarefa != null) {
@@ -38,6 +47,11 @@ public class TarefaController {
         }
     }
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Lista todas as tarefas por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa encontrada"),
+            @ApiResponse(responseCode = "400", description = "Tarefa nao encotarada")
+    })
     public ResponseEntity<?> listarTarefaID(@PathVariable Long id){
         TarefasDTO tarefa = tarefaService.listarTarefaPorId(id);
         if(tarefa != null){
@@ -48,6 +62,11 @@ public class TarefaController {
         }
     }
     @GetMapping("/listar/status/{status}")
+    @Operation(summary = "Lista todas as tarefas por status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa encontrada"),
+            @ApiResponse(responseCode = "400", description = "Tarefa nao encontrada")
+    })
     public ResponseEntity<?> listarPorStatus(@PathVariable String status){
         List<TarefasDTO> tarefa  = tarefaService.listarPorStatus(status);
         if (tarefa != null){
@@ -58,6 +77,11 @@ public class TarefaController {
         }
     }
     @PutMapping("/alterar/{id}")
+    @Operation(summary = "Altera o tarefa por id" , description = "Rota altera o tarefa por id e insere no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa alterado com sucesso"),
+            @ApiResponse(responseCode = "404" , description = "Tarefa nao encontrado")
+    })
     public ResponseEntity<String> alterarTarefa(@PathVariable Long id , @RequestBody TarefasDTO tarefasDTO){
 
         if (tarefaService.listarTarefaPorId(id) != null){
@@ -70,6 +94,7 @@ public class TarefaController {
         }
     }
     @DeleteMapping("/deletar/{id}")
+    @Operation(summary = "Deleta o tarefa por id" , description = "Faz a deleçao de um tarefa por id passado na requisiçao")
     public ResponseEntity<String> deletarPorId(@PathVariable Long id){
         if(tarefaService.listarTarefaPorId(id) != null){
             tarefaService.deletarTarefa(id);
@@ -82,6 +107,11 @@ public class TarefaController {
 
 
     @PostMapping("/criar")
+    @Operation(summary = "Criar um novo tarefa" , description = "Rota cria um novo tarefa e insere no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Tarefa criado com sucesso"),
+            @ApiResponse(responseCode = "400" , description = "Erro na criaçao do tarefa")
+    })
     public ResponseEntity<String> criarTarefa(@RequestBody TarefasDTO tarefasDTO){
         TarefasDTO novaTarefa = tarefaService.criarTarefa(tarefasDTO);
         return ResponseEntity.status(HttpStatus.CREATED)
